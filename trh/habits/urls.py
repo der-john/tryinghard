2152966064df228cf8f75df1ebda0c85628e159f
John Schmid <bt0404@btb.org>
2025-01-08 11:48:45 +0100

    enable user to set and unset entries

--- trh/habits/urls.py
+++ trh/habits/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     # ex: /trh/5/
     path("<int:h_id>/", views.detail, name="detail"),
+    path("<int:h_id>/setentry", views.setentry, name="setentry"),
 ]
--- trh/habits/views.py
+++ trh/habits/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse, Http404
+import datetime
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
 
@@ -13,14 +14,33 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+
 def detail(request, h_id):
     try:
         habit = Habit.objects.get(pk=h_id)
         entries = Entry.objects.filter(habit=habit)
         entry_dates = [[e.date.year, e.date.month, e.date.day] for e in entries]
     except Habit.DoesNotExist:
-        raise Http404("Question does not exist")
+        raise Http404("Habit / Streak does not exist")
     return render(request, "habits/detail.html",
-                  { "habit": habit,
-                    "entry_dates": entry_dates
-                   })
+        { "habit": habit, "entry_dates": entry_dates, "yr": request.GET.get("yr"), "mo": request.GET.get("mo") })
+
+
+def setentry(request, h_id):
+    try:
+        habit = Habit.objects.get(pk=h_id)
+        year_str = request.POST["year"]
+        month_str = request.POST["month"]
+        day = int(request.POST["day"])
+        e_date = datetime.date(int(year_str), int(month_str), day)
+        entries = Entry.objects.filter(habit=habit).filter(date=e_date)
+
+        if len(entries) == 0:
+            new_entry = Entry.objects.create(habit=habit, date=e_date)
+            Entry.save(new_entry)
+        else:
+            entries.delete()
+
+    except Habit.DoesNotExist:
+        raise Http404("Habit / Streak does not exist")
+    return HttpResponseRedirect("/trh/" + str(h_id) + "?yr=" + year_str + "&mo=" + month_str)
